1329c9fda5111241ef375598d8ceaecfaf54f781
Ilya Saushin <esinilyadev@gmail.com>
2025-02-12 20:24:59 +0300

    Added availability to update bearer jwt tokens

--- app_update/src/main/kotlin/com/earldev/app_update/di/AppUpdaterComponentHolder.kt
+++ app_update/src/main/kotlin/com/earldev/app_update/di/AppUpdaterComponentHolder.kt
@@ -1,7 +1,6 @@
 package com.earldev.app_update.di
 
 import android.content.Context
-import android.util.Log
 import com.earldev.app_update.api.UpdaterApi
 
 internal object AppUpdaterComponentHolder {
@@ -9,7 +8,6 @@ internal object AppUpdaterComponentHolder {
     private var component: AppUpdaterComponent? = null
 
     fun init(context: Context)  {
-        Log.d("TAg1", "init: ")
         component = null
         component = DaggerAppUpdaterComponent.builder()
             .context(context)
